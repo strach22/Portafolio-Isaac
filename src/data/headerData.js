694b2daa02e7549b8376bca3aaa2b51d4fd89490
Isaac <isaacandrespozo@gmail.com>
2022-04-04 20:53:21 -0500

    cv pdf added

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,4 +1,4 @@
-import resume from "../assets/pdf/resume.pdf";
+import Isaac_Pozo_CV from "../assets/pdf/Isaac Pozo cv Desarrollo web.pdf";
 
 export const headerData = {
   name: "Isaac Pozo",
@@ -7,5 +7,5 @@ export const headerData = {
     "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
   image:
     "https://scontent.fuio1-1.fna.fbcdn.net/v/t1.6435-9/56140487_2144572685610999_211407455769329664_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=174925&_nc_eui2=AeE_kSuTNslGXNQjiuRYOYytW-CKwcAORjRb4IrBwA5GNKMhShqjK3JR8jBMkosuhvkwvT84Zw59fDT8FHibYRAe&_nc_ohc=0_gSf6l9Y0oAX9Y48Zx&_nc_ht=scontent.fuio1-1.fna&oh=00_AT-8UdIs5xMBMH_HnQF1Jb7s3Md5oIzmFxHOQRGA9GFhSA&oe=626A789D",
-  resumePdf: resume,
+  resumePdf: Isaac_Pozo_CV,
 };
